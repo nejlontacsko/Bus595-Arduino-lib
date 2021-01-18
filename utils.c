63ca69d407d05c5577d46cfc0df6db9709d50583
Fodor Attila <atti@fodyweb.hu>
2021-01-18 20:53:26 +0100

    Add files via upload

---
+++ utils.c
@@ -0,0 +1,13 @@
+#include "utils.h"
+
+int _find(char* haystack, char needle)
+{
+	int ret = 0;
+	for (int i = 0; haystack[i] > 0; i++)
+		if (haystack[i] == needle)
+		{
+			ret = i;
+			break;			
+		}
+	return ret;
+}
---
+++ utils.h
@@ -0,0 +1,6 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+int _find(char*, char);
+
+#endif
